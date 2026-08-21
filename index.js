8205c8ae0f59fe7c0a4beddcc408f46d51ea5681
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-21 23:55:24 +0600

    fix warning

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+import "dotenv/config";
 import app from "./src/app.js";
-export default app;
+import { connectDB } from "./src/config/db.js";
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+  try {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error("Server Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
