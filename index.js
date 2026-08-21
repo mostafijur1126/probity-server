fedd9d35bd4b0a7bec459bb8dd83bb073d00eafe
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-22 00:27:00 +0600

    fix deployment issue

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-import "dotenv/config";
 import app from "./src/app.js";
-import { connectDB } from "./src/config/db.js";
-
-let isConnected = false;
-
-export default async function handler(req, res) {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-
-    return app(req, res);
-  } catch (error) {
-    console.error("Server Error:", error);
-
-    return res.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-}
+export default app;
--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import { projectCollection } from "./config/db.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
